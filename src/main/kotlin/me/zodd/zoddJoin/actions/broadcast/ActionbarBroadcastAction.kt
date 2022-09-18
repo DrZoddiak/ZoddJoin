b953d0b174cc715c86835a24903213579cf325ff
Jesse McKee <jessemckee97@gmail.com>
2022-09-18 00:09:31 -0500

    Optimize Imports

--- src/main/kotlin/me/zodd/zoddJoin/actions/broadcast/ActionbarBroadcastAction.kt
+++ src/main/kotlin/me/zodd/zoddJoin/actions/broadcast/ActionbarBroadcastAction.kt
@@ -3,7 +3,6 @@ package me.zodd.zoddJoin.actions.broadcast
 import com.google.auto.service.AutoService
 import me.zodd.zoddJoin.actions.AbstractAction
 import me.zodd.zoddJoin.actions.Action
-import me.zodd.zoddJoin.not
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.entity.living.player.server.ServerPlayer
 
