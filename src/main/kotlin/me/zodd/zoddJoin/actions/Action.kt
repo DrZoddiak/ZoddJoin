2bd9e1c54cee80b20b1d26a81b1f425f3553d43b
Jesse McKee <jessemckee97@gmail.com>
2022-09-18 00:09:19 -0500

    override equals

--- src/main/kotlin/me/zodd/zoddJoin/actions/Action.kt
+++ src/main/kotlin/me/zodd/zoddJoin/actions/Action.kt
@@ -24,4 +24,18 @@ abstract class AbstractAction : Action {
     override fun toString(): String {
         return this.id
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Action) return false
+
+        if (this.id == other.id)
+            return true
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
